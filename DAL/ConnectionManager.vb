f6887327efebb3626fdb3a78339953e1ea9f1695
IsaacEstrada <isaacestrada168@gmail.com>
2025-06-04 21:37:47 -0600

    Creacion del archivo de conexion a la base de datos.

---
+++ DAL/ConnectionManager.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Data.SqlClient
+
+Public Class ConnectionManager
+
+    Private Shared _connection As SqlConnection 'Declaro una variable de tipo SqlConnection
+
+
+    Public Shared Function GetConnection() As SqlConnection
+
+        If _connection Is Nothing Then
+
+            Dim connectionString As String = "Data Source=DESKTOP-JSC6DST\SQLEXPRESS;Initial Catalog=db_sistemas;User ID=sa;Password=Coco14.negrita90;TrustServerCertificate=True"
+
+            _connection = New SqlConnection(connectionString)
+
+        End If
+
+
+        If _connection.State <> ConnectionState.Open Then _connection.Open() 'Abro conexion si no esta abierta
+
+        Return _connection 'Retorno la conexion abierta
+
+    End Function
+
+
+    Public Shared Sub CloseConnection()
+
+        If _connection IsNot Nothing AndAlso _connection.State <> ConnectionState.Closed Then
+            _connection.Close()
+            _connection = Nothing
+        End If
+
+    End Sub
+
+
+End Class
